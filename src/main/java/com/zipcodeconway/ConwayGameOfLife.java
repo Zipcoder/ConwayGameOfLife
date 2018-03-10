225e07af33b350145bc6b257b39a7a0a12f008c3
vvmk <vincentmasiello@gmail.com>
2018-03-10 13:00:34 -0500

    Cell is alive logic, simulator

--- src/main/java/com/zipcodeconway/ConwayGameOfLife.java
+++ src/main/java/com/zipcodeconway/ConwayGameOfLife.java
@@ -1,16 +1,22 @@
 package com.zipcodeconway;
 
 public class ConwayGameOfLife {
+    private static final int fDEFAULT_GAME_SIZE = 50;
+    private int size;
+    private int[][] startingGen; //maybe not need
 
     public ConwayGameOfLife(Integer dimension) {
-     }
+        size = dimension;
+    }
 
     public ConwayGameOfLife(Integer dimension, int[][] startmatrix) {
+        size = dimension;
+        copyAndZeroOut(startingGen, startmatrix);
     }
 
     public static void main(String[] args) {
-        ConwayGameOfLife sim = new ConwayGameOfLife(50);
-        int[][] endingWorld = sim.simulate(50);
+        ConwayGameOfLife sim = new ConwayGameOfLife(fDEFAULT_GAME_SIZE);
+        int[][] endingWorld = sim.simulate(fDEFAULT_GAME_SIZE);
     }
 
     // Contains the logic for the starting scenario.
@@ -21,6 +27,19 @@ public class ConwayGameOfLife {
     }
 
     public int[][] simulate(Integer maxGenerations) {
+        int[][] currentGen = new int[1][1];
+        copyAndZeroOut(startingGen, currentGen);
+
+        int[][] nextGen = new int[size][size];
+
+        for (int gen = 0; gen < maxGenerations; gen++) {
+            for (int i = 0; i < size; i++) {
+                for (int j = 0; j < size; j++) {
+                     nextGen[i][j] = isAlive(i, j, currentGen);
+                }
+            }
+            copyAndZeroOut(currentGen, nextGen);
+        }
         return new int[1][1];
     }
 
@@ -38,40 +57,40 @@ public class ConwayGameOfLife {
 		Any dead cell with exactly three live neighbours cells will come to life.
 	*/
     private int isAlive(int row, int col, int[][] world) {
-      return getLeft(row, col, world) +
+        int liveNeighbors =  getLeft(row, col, world) +
                 getRight(row, col, world) +
-              getAboveRow(row, col, world) +
-              getBelowRow(row, col, world);
+                getTop(row, col, world) +
+                getBottom(row, col, world);
 
+        switch (liveNeighbors) {
+            case 2:
+                return world[row][col];
+            case 3:
+                return 1;
+            default:
+                return 0;
+        }
     }
 
     // return 1 if the cell to the left is alive, 0 if not
     private int getLeft(int row, int col, int[][] world) {
-        return -1;
+        int lC = (col == 0) ? size - 1 : (col - 1);
+        return world[row][lC];
     }
 
     // return 1 if the cell to the right is alive, 0 if not
     private int getRight(int row, int col, int[][] world) {
-        return -1;
+        int rC = (col == size - 1) ? 0 : (row + 1);
+        return world[rC][col];
     }
 
     private int getTop(int row, int col, int[][] world) {
-        return -1;
+        int tR = (row == 0) ? size - 1 : (row - 1);
+        return world[tR][col] + getLeft(tR, col, world) + getRight(tR, col, world);
     }
 
     private int getBottom(int row, int col, int[][] world) {
-        return -1;
+        int bR = (row == size - 1) ? 0 : (row + 1);
+        return world[bR][col] + getLeft(bR, col, world) + getRight(bR, col, world);
     }
-
-    // return the live cells in the row above
-    private int getAboveRow(int row, int col, int[][] world) {
-        return -1;
-    }
-
-    // return the live cells in the row below
-    private int getBelowRow(int row, int col, int[][] world) {
-        return -1;
-    }
-
-
 }
