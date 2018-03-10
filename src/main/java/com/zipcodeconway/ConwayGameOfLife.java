88828ea501ce5c3909b9fe361630bf58005bd889
Eric Barnaba <ericbarnaba@zipcoders-MacBook-Pro-25.local>
2018-03-10 14:51:55 -0500

    saving

--- src/main/java/com/zipcodeconway/ConwayGameOfLife.java
+++ src/main/java/com/zipcodeconway/ConwayGameOfLife.java
@@ -1,11 +1,23 @@
 package com.zipcodeconway;
 
 public class ConwayGameOfLife {
+    SimpleWindow displayWindow;
+    int[][] currentGeneration;
+    int[][] nextGeneration;
+    int generations;
+    int edge;
+
 
     public ConwayGameOfLife(Integer dimension) {
+        this.displayWindow = new SimpleWindow(dimension);
+        this.edge = dimension-1;
      }
 
     public ConwayGameOfLife(Integer dimension, int[][] startmatrix) {
+        this.displayWindow = new SimpleWindow(dimension);
+        this.currentGeneration = startmatrix;
+        this.edge = dimension-1;
+
     }
 
     public static void main(String[] args) {
@@ -21,6 +33,8 @@ public class ConwayGameOfLife {
     }
 
     public int[][] simulate(Integer maxGenerations) {
+        this.displayWindow.display(currentGeneration,generations);
+        this.displayWindow.sleep(125);
         return new int[1][1];
     }
 
@@ -38,6 +52,26 @@ public class ConwayGameOfLife {
 		Any dead cell with exactly three live neighbours cells will come to life.
 	*/
     private int isAlive(int row, int col, int[][] world) {
-        return 0;
+        int liveCells=0;
+        for(int i = row-1; i<row+1; i++){
+            int iLooker;
+            if(i<0) iLooker = edge;
+            else if (i>edge) iLooker =0;
+            else iLooker = i;
+
+            for(int j = col-1; j<col +1; j++){
+                int jLooker;
+                if(j<0) jLooker = edge;
+                else if (j>edge) jLooker =0;
+                else jLooker = j;
+
+                if(world[iLooker][jLooker] == 1) liveCells++;
+            }
+        }
+
+        if(liveCells == 3) return 1;
+        else if(liveCells == 4) return world[row][col];
+        else return 0;
+
     }
 }
