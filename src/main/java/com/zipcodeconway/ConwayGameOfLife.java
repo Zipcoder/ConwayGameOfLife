4cec434e270105cf4b69c8fc9a46eacf67615550
Patrick Glavin <patjglavin@gmail.com>
2018-03-11 13:27:58 -0400

    refactor getRandomStart as static, begin parallax check

--- src/main/java/com/zipcodeconway/ConwayGameOfLife.java
+++ src/main/java/com/zipcodeconway/ConwayGameOfLife.java
@@ -1,11 +1,27 @@
 package com.zipcodeconway;
 
 public class ConwayGameOfLife {
+    private int size;
+    private SimpleWindow window;
+    private int[][] board;
+
+    public int getSize() {
+        return size;
+    }
+
+    public ConwayGameOfLife(){
+        this(50);
+    }
 
     public ConwayGameOfLife(Integer dimension) {
+        this(dimension, ConwayGameOfLife.createRandomStart(dimension));
      }
 
-    public ConwayGameOfLife(Integer dimension, int[][] startmatrix) {
+    public ConwayGameOfLife(Integer dimension, int[][] startMatrix) {
+        this.window = new SimpleWindow(dimension);
+        this.board = startMatrix;
+        this.size = dimension;
+        window.display(startMatrix, 1);
     }
 
     public static void main(String[] args) {
@@ -16,17 +32,33 @@ public class ConwayGameOfLife {
     // Contains the logic for the starting scenario.
     // Which cells are alive or dead in generation 0.
     // allocates and returns the starting matrix of size 'dimension'
-    private int[][] createRandomStart(Integer dimension) {
-        return new int[1][1];
+    public static int[][] createRandomStart(Integer dimension) {
+        int[][] rando = new int[dimension][dimension];
+        for (int i = 0; i < dimension; i++){
+            for (int j = 0; j < dimension; j++){
+                rando[i][j] = (int)Math.round(Math.random());
+            }
+        }
+        return rando;
     }
 
     public int[][] simulate(Integer maxGenerations) {
-        return new int[1][1];
+        int currentGen = 1;
+        int[][] currentBoard = this.board;
+        while (currentGen < maxGenerations){
+            for (int i = 0; i < size; i++){
+                for (int j = 0; j < size; j++){
+                    currentBoard[i][j] = isAlive(i, j, currentBoard);
+                }
+            }
+        }
+        return currentBoard;
     }
 
     // copy the values of 'next' matrix to 'current' matrix,
     // and then zero out the contents of 'next' matrix
     public void copyAndZeroOut(int [][] next, int[][] current) {
+
     }
 
     // Calculate if an individual cell should be alive in the next generation.
@@ -37,7 +69,43 @@ public class ConwayGameOfLife {
 		Any live cell with two or three live neighbours lives, unchanged, to the next generation.
 		Any dead cell with exactly three live neighbours cells will come to life.
 	*/
-    private int isAlive(int row, int col, int[][] world) {
+    public int isAlive(int row, int col, int[][] world) {
+        int neighborCount = countNeighbors(row, col, world);
+        if (world[row][col] == 1) {
+            if (neighborCount < 2) return 0;
+            if (neighborCount == 2 || neighborCount == 3) return 1;
+            if (neighborCount > 3) return 0;
+        } else {
+            if (neighborCount == 3) return 1;
+        }
         return 0;
     }
+
+    private int countNeighbors(int row, int col, int[][] world) {
+        int neighborCount = 0;
+        for (int i = row-1; i <= row+1; i++){
+            for (int j = col -1; j <= col + 1; j++){
+                if (world[i][j] == 1 && !(i == row && j == col)) {
+                    neighborCount++;
+                } else {
+                }
+            }
+        }
+        return neighborCount;
+    }
+
+    public int[][] neighborCoordGenerator(int row, int col){
+        int[][] subworld = new int[2][3];
+        for (int i = row-1; i <= row+1; i++){
+
+        }
+        return subworld;
+    }
+
+    public int loopCheck(int coord){
+        if (coord < 0) return this.size - coord;
+        if (coord > this.size) return coord - this.size;
+        return coord;
+    }
+
 }
--- src/test/java/com/zipcodeconway/ConwayGameOfLifeTest.java
+++ src/test/java/com/zipcodeconway/ConwayGameOfLifeTest.java
@@ -1,10 +1,13 @@
 package com.zipcodeconway;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class ConwayGameOfLifeTest {
+
+    ConwayGameOfLife game;
 
     @Test
     public void runTest1() {
@@ -42,5 +45,130 @@ public class ConwayGameOfLifeTest {
         ConwayGameOfLife sim = new ConwayGameOfLife(5, start);
         int[][] results = sim.simulate(10);
         assertTrue(java.util.Arrays.deepEquals(results, expected));
+    }
+
+    @Test
+    public void randomStartTest(){
+        ConwayGameOfLife sim = new ConwayGameOfLife(50);
+    }
+
+    @Test
+    public void isAlive2NeighborTest(){
+        int[][] start = {
+                {0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0},
+                {0, 1, 1, 1, 0},
+                {0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0}};
+        game = new ConwayGameOfLife(5, start);
+        int expected = 1;
+        int actual = game.isAlive(2, 2, start);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void isAlive0NeighborTest(){
+        int[][] start = {
+                {0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0}};
+        game = new ConwayGameOfLife(5, start);
+        int expected = 0;
+        int actual = game.isAlive(2, 2, start);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void isAlive3NeighborTest(){
+        int[][] start = {
+                {0, 0, 0, 0, 0},
+                {0, 0, 1, 0, 0},
+                {0, 1, 0, 0, 0},
+                {0, 1, 0, 0, 0},
+                {0, 0, 0, 0, 0}};
+        game = new ConwayGameOfLife(5, start);
+        int expected = 1;
+        int actual = game.isAlive(2, 2, start);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void isAlive4NeighborTest(){
+        int[][] start = {
+                {0, 0, 0, 0, 0},
+                {0, 0, 1, 0, 0},
+                {0, 1, 1, 1, 0},
+                {0, 0, 1, 0, 0},
+                {0, 0, 0, 0, 0}};
+        game = new ConwayGameOfLife(5, start);
+        int expected = 0;
+        int actual = game.isAlive(2, 2, start);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void isAliveReproductionTest(){
+        int[][] start = {
+                {0, 0, 0, 0, 0},
+                {0, 1, 1, 0, 0},
+                {0, 1, 0, 0, 0},
+                {0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0}};
+        game = new ConwayGameOfLife(5, start);
+        int expected = 1;
+        int actual = game.isAlive(2, 2, start);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void isAliveEdgeTest(){
+        int[][] start = {
+                {0, 0, 0, 0, 0},
+                {0, 1, 1, 0, 0},
+                {0, 1, 0, 0, 0},
+                {0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0}};
+        game = new ConwayGameOfLife(5, start);
+        int expected = 1;
+        int actual = game.isAlive(5, 2, start);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void subworldCenterTest(){
+        int[][] start = {
+                {0, 0, 0, 0, 0},
+                {0, 1, 1, 0, 0},
+                {0, 1, 0, 0, 0},
+                {0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0}};
+        game = new ConwayGameOfLife(5, start);
+        int[][] expected = {
+                {2, 3, 4},
+                {2, 3, 4}
+        };
+        int[][] actual = game.neighborCoordGenerator(3, 3);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void loopCheck(){
+        game = new ConwayGameOfLife(5);
+        Assert.assertEquals(game.loopCheck(3), 3);
+    }
+
+    @Test
+    public void loopCheckEdge(){
+        game = new ConwayGameOfLife(5);
+        System.out.println(game.getSize());
+        Assert.assertEquals(5, game.loopCheck(-1));
+    }
+
+    @Test
+    public void loopCheckEdge2(){
+        game = new ConwayGameOfLife(5);
+        Assert.assertEquals(0, game.loopCheck(6));
     }
 }
