69dd19a1c53d48d3c6b90adf916e4b1031aedee5
Joe Hendricks <joehendricks@zipcoders-MacBook-Pro-92.local>
2018-03-12 13:41:18 -0400

    isAlive tested and working

--- src/main/java/com/zipcodeconway/ConwayGameOfLife.java
+++ src/main/java/com/zipcodeconway/ConwayGameOfLife.java
@@ -1,11 +1,24 @@
 package com.zipcodeconway;
 
 public class ConwayGameOfLife {
+    //private SimpleWindow displayWindow;
+    private Integer edge;
+    int[][] currentGen;
+    int[][] nextGen;
+
 
     public ConwayGameOfLife(Integer dimension) {
+        //this.displayWindow = new SimpleWindow(dimension);
+        this.edge = dimension -1;
+        this.nextGen = new int [dimension][dimension;
      }
 
     public ConwayGameOfLife(Integer dimension, int[][] startmatrix) {
+        //this.displayWindow = new SimpleWindow(dimension);
+        this.edge = dimension -1;
+        this.currentGen = startmatrix;
+        this.nextGen = new int [dimension][dimension;
+
     }
 
     public static void main(String[] args) {
@@ -23,11 +36,12 @@ public class ConwayGameOfLife {
     public int[][] simulate(Integer maxGenerations) {
         return new int[1][1];
     }
-
     // copy the values of 'next' matrix to 'current' matrix,
     // and then zero out the contents of 'next' matrix
     public void copyAndZeroOut(int [][] next, int[][] current) {
+
     }
+
 
     // Calculate if an individual cell should be alive in the next generation.
     // Based on the game logic:
@@ -37,7 +51,39 @@ public class ConwayGameOfLife {
 		Any live cell with two or three live neighbours lives, unchanged, to the next generation.
 		Any dead cell with exactly three live neighbours cells will come to life.
 	*/
-    private int isAlive(int row, int col, int[][] world) {
-        return 0;
+    protected int isAlive(int row, int col, int[][] world) {
+    //is alive in next generation//
+
+        int nextGenLives;
+        int counter = 0;
+
+        for(int i = row - 1; i <= row + 1; i++) {
+            int iSeeker = i;
+            if(i < 0){
+                iSeeker = edge;
+            } else if(i > edge){
+                iSeeker = 0;
+            }
+
+            for(int j = col - 1; j <= col + 1; j++){
+                int jSeeker = j;
+                if(j < 0){
+                    jSeeker = edge;
+                } else if(j > edge){
+                    jSeeker = 0;
+                }
+                if(world[iSeeker][jSeeker] == 1) counter++;
+            }
+        }
+
+        if (counter == 3){
+            nextGenLives = 1;
+        } else if (counter == 4){
+            nextGenLives = world[row][col];
+        } else {
+            nextGenLives = 0;
+        }
+
+        return nextGenLives;
     }
 }
