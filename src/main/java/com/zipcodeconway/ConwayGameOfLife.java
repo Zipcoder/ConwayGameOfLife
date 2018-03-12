fe4729af22e8c183ef85baf713222fec9be54cac
Vincent Gasbarro <vincentgasbarro@zipcoders-MacBook-Pro.local>
2018-03-12 15:17:49 -0400

    finished

--- src/main/java/com/zipcodeconway/ConwayGameOfLife.java
+++ src/main/java/com/zipcodeconway/ConwayGameOfLife.java
@@ -2,10 +2,24 @@ package com.zipcodeconway;
 
 public class ConwayGameOfLife {
 
+    int edge;
+    int[][] currentGen;
+    int[][] nextGen;
+    SimpleWindow displayWindow;
+
+
     public ConwayGameOfLife(Integer dimension) {
-     }
+        edge = dimension - 1;
+        nextGen = new int[dimension][dimension];
+        displayWindow = new SimpleWindow(dimension);
+        currentGen = createRandomStart(dimension);
+    }
 
     public ConwayGameOfLife(Integer dimension, int[][] startmatrix) {
+        edge = dimension - 1;
+        currentGen = startmatrix;
+        nextGen = new int[dimension][dimension];
+        displayWindow = new SimpleWindow(dimension);
     }
 
     public static void main(String[] args) {
@@ -17,16 +31,52 @@ public class ConwayGameOfLife {
     // Which cells are alive or dead in generation 0.
     // allocates and returns the starting matrix of size 'dimension'
     private int[][] createRandomStart(Integer dimension) {
-        return new int[1][1];
+
+        currentGen = new int[dimension][dimension];
+
+        for (int i = 0; i < dimension; i++) {
+            for (int j = 0; j < dimension; j++) {
+                if ((Math.random() * 100) > 28) {
+                    currentGen[i][j] = 0;
+                } else {
+                    currentGen[i][j] = 1;
+                }
+            }
+        }
+
+        return currentGen;
     }
 
     public int[][] simulate(Integer maxGenerations) {
-        return new int[1][1];
+
+        int generations = 0;
+
+        while (generations <= maxGenerations) {
+            this.displayWindow.display(currentGen, generations);
+            this.displayWindow.sleep(75);
+            copyAndZeroOut(nextGen, currentGen);
+            generations++;
+        }
+
+        return currentGen;
     }
 
     // copy the values of 'next' matrix to 'current' matrix,
     // and then zero out the contents of 'next' matrix
     public void copyAndZeroOut(int [][] next, int[][] current) {
+
+        for (int i = 0; i < current.length; i++) {
+            for (int j = 0; j< current.length; j++) {
+                next[i][j] = isAlive(i, j, current);
+            }
+        }
+
+        for (int i = 0; i < current.length; i++) {
+            for (int j = 0; j < current.length; j++) {
+                current[i][j] = next[i][j];
+                next[i][j] = 0;
+            }
+        }
     }
 
     // Calculate if an individual cell should be alive in the next generation.
@@ -37,7 +87,46 @@ public class ConwayGameOfLife {
 		Any live cell with two or three live neighbours lives, unchanged, to the next generation.
 		Any dead cell with exactly three live neighbours cells will come to life.
 	*/
-    private int isAlive(int row, int col, int[][] world) {
-        return 0;
+    protected int isAlive(int row, int col, int[][] world) {
+
+        int iLooker;
+        int jLooker;
+        int count = 0;
+
+        for (int i = row-1; i < row+2; i++) {
+
+            if (i > edge) {
+                iLooker = 0;
+            } else if (i < 0) {
+                iLooker = edge;
+            } else {
+                iLooker = i;
+            }
+
+            for (int j = col-1; j < col+2; j++) {
+
+                if (j > edge) {
+                    jLooker = 0;
+                } else if (j < 0) {
+                    jLooker = edge;
+                } else {
+                    jLooker = j;
+                }
+
+                if (world[iLooker][jLooker] == 1) {
+                    count++;
+                }
+            }
+        }
+
+        if (count == 3) {
+            return 1;
+        } else if (count <3 || count > 4) {
+            return 0;
+        }
+
+        return world[row][col];
     }
+
+
 }
--- src/test/java/com/zipcodeconway/ConwayGameOfLifeTest.java
+++ src/test/java/com/zipcodeconway/ConwayGameOfLifeTest.java
@@ -1,5 +1,6 @@
 package com.zipcodeconway;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -43,4 +44,53 @@ public class ConwayGameOfLifeTest {
         int[][] results = sim.simulate(10);
         assertTrue(java.util.Arrays.deepEquals(results, expected));
     }
+
+    @Test
+    public void isAliveTest1() {
+        ConwayGameOfLife sim = new ConwayGameOfLife(5);
+        int row = 2;
+        int col = 2;
+        int[][] world = {
+                {0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0},
+                {0, 1, 1, 1, 0},
+                {0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0}};
+        int expected = 1;
+        int actual = sim.isAlive(row, col, world);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void isAliveTest2() {
+        ConwayGameOfLife sim = new ConwayGameOfLife(5);
+        int row = 1;
+        int col = 4;
+        int[][] world = {
+                {1, 0, 0, 1, 0},
+                {1, 1, 1, 0, 0},
+                {0, 1, 1, 1, 0},
+                {1, 1, 0, 1, 1},
+                {1, 0, 0, 1, 1}};
+        int expected = 0;
+        int actual = sim.isAlive(row, col, world);
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void isAliveTest3() {
+        ConwayGameOfLife sim = new ConwayGameOfLife(5);
+        int row = 4;
+        int col = 2;
+        int[][] world = {
+                {1, 0, 0, 1, 0},
+                {1, 1, 1, 0, 0},
+                {0, 1, 1, 1, 0},
+                {1, 0, 0, 1, 1},
+                {1, 0, 0, 1, 1}};
+        int expected = 1;
+        int actual = sim.isAlive(row, col, world);
+        Assert.assertEquals(expected, actual);
+    }
+
 }
